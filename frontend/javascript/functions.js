f9ba61f0fd169a989c159d88c0528a7aaf8e5bbf
Gustavo <44278997+GustavoHBS@users.noreply.github.com>
2019-01-27 12:28:13 -0200

    Alterando o layout do grafico
    Alterando as cores do grafico.

---
+++ frontend/javascript/functions.js
@@ -0,0 +1,96 @@
+function loadGraph(coin = "Dollar"){
+	$("#graph").remove();
+	$.getJSON(
+		'https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/usdeur.json',
+		function (data) {
+	
+		Highcharts.chart('graph', {
+			
+			chart: {
+				backgroundColor: {
+					linearGradient: [0],
+					stops: [
+						[0, 'rgb(61, 61, 61)']
+					]
+				},
+				zoomType: 'x'
+			},
+			title: {
+				style: {
+					color: "white"
+				},
+				text: `Conversão de ${coin} para Real`
+			},
+			subtitle: {
+				style: {
+					color: "white"
+				},
+				text: document.ontouchstart === undefined ?
+					'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+			},
+			xAxis: {
+				type: 'datetime',
+				style: {
+					color: "white"
+				},
+				labels: {
+					style: {
+						color: 'white'
+					}
+				}
+			},
+			yAxis: {
+				title: {
+					text: 'Exchange rate',
+					style: {
+						color: "white"
+					},
+				},
+				labels: {
+					style: {
+						color: 'white'
+					}
+				}
+			},
+			legend: {
+				enabled: false
+			},
+			plotOptions: {
+				area: {
+					fillColor: {
+					linearGradient: {
+						x1: 0,
+						y1: 0,
+						x2: 0,
+						y2: 1
+					},
+					stops: [
+						[0, 'rgb(255, 255, 255)'],
+						[1, 'rgb(0, 0, 0)']
+					]
+				},
+				marker: {
+					radius: 2
+				},
+				lineWidth: 1,
+				states: {
+					hover: {
+						lineWidth: 1
+					}
+				},
+				threshold: null
+				},
+				series: {
+					color: '#e47707'
+				}
+			},	
+			series: [{
+				type: 'area',
+				name: `${coin}`,
+				data: data
+			}]
+		});
+		}
+	);
+}
+loadGraph();
